89556b9893653242c3caf5275dcd9fc58bf773b8
tsteelematc <tsteele@madisoncollege.edu>
2025-11-19 21:54:38 -0600

    Gonna try to use fork of eks-cluster too

--- ch3/tofu/modules/eks-cluster/main.tf
+++ ch3/tofu/modules/eks-cluster/main.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "cluster" {
   name     = var.name
-  role_arn = aws_iam_role.cluster.arn
+  role_arn = data.aws_iam_role.lab_role.arn
   version  = var.eks_version
 
   vpc_config {
@@ -18,7 +18,7 @@ resource "aws_eks_cluster" "cluster" {
 resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = var.name
-  node_role_arn   = aws_iam_role.node_group.arn
+  node_role_arn   = data.aws_iam_role.lab_role.arn
   subnet_ids      = data.aws_subnets.default.ids
   instance_types  = [var.instance_type]
 
@@ -42,56 +42,28 @@ resource "aws_eks_node_group" "nodes" {
   }
 }
 
-resource "aws_iam_role" "cluster" {
-  name               = "${var.name}-control-plane"
-  assume_role_policy = data.aws_iam_policy_document.cluster_assume_role.json
-}
-
-data "aws_iam_policy_document" "cluster_assume_role" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
-}
-
-resource "aws_iam_role" "node_group" {
-  name               = "${var.name}-worker-nodes"
-  assume_role_policy = data.aws_iam_policy_document.node_assume_role.json
-}
-
-data "aws_iam_policy_document" "node_assume_role" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
+  role       = data.aws_iam_role.lab_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node_group.name
+  role       = data.aws_iam_role.lab_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.node_group.name
+  role       = data.aws_iam_role.lab_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node_group.name
+  role       = data.aws_iam_role.lab_role.name
 }
 
 resource "aws_eks_addon" "eks_pod_identity_agent" {
