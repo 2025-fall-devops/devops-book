704f9dbfe30aa48088ab725996e5d7b74a95fcdc
tsteelematc <tsteele@madisoncollege.edu>
2025-11-19 21:36:27 -0600

    Maybe getting further
    Fun but geesh...

--- ch3/tofu/modules/lambda/outputs.tf
+++ ch3/tofu/modules/lambda/outputs.tf
@@ -13,16 +13,6 @@ output "function_version" {
   value       = aws_lambda_function.function.version
 }
 
-output "iam_role_arn" {
-  description = "The ARN of the IAM role attached to the Lambda function"
-  value       = aws_iam_role.lambda.arn
-}
-
-output "iam_role_name" {
-  description = "The name of the IAM role attached to the Lambda function"
-  value       = aws_iam_role.lambda.name
-}
-
 output "function_url" {
   description = "The Lambda function URL (if enabled)"
   value       = length(aws_lambda_function_url.url) > 0 ? aws_lambda_function_url.url[0].function_url : null
