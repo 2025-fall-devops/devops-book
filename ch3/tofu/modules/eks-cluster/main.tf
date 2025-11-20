9449051f2de9ccb7bfb432d9965dc694897734dc
tsteelematc <tsteele@madisoncollege.edu>
2025-11-19 22:04:22 -0600

    assume labrole is good so don't try to do other stuffy-stuff

--- ch3/tofu/modules/eks-cluster/main.tf
+++ ch3/tofu/modules/eks-cluster/main.tf
@@ -6,13 +6,6 @@ resource "aws_eks_cluster" "cluster" {
   vpc_config {
     subnet_ids = data.aws_subnets.default.ids
   }
-
-  # Ensure that IAM Role permissions are created before and deleted after
-  # the EKS Cluster. Otherwise, EKS will not be able to properly delete
-  # EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy
-  ]
 }
 
 resource "aws_eks_node_group" "nodes" {
@@ -28,15 +21,6 @@ resource "aws_eks_node_group" "nodes" {
     desired_size = var.min_worker_nodes
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after
-  # the EKS Node Group. Otherwise, EKS will not be able to properly
-  # delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-  ]
-
   lifecycle {
     ignore_changes = [scaling_config[0].desired_size]
   }
@@ -44,26 +28,6 @@ resource "aws_eks_node_group" "nodes" {
 
 data "aws_iam_role" "lab_role" {
   name = "LabRole"
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = data.aws_iam_role.lab_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = data.aws_iam_role.lab_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = data.aws_iam_role.lab_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = data.aws_iam_role.lab_role.name
 }
 
 resource "aws_eks_addon" "eks_pod_identity_agent" {
